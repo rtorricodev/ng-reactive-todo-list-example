a25b3a73ac559575fb7d4cfe79a144b58c183814
rodrigotorrico <rodrigo.torricodelcastillo@hotmail.com>
2019-01-08 14:34:37 -0400

    updated principale module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoCreateComponent } from './components/todo-create/todo-create.component';
+import { TodoReadComponent } from './components/todo-read/todo-read.component';
+
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './reducers/todo.reducer';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoCreateComponent,
+    TodoReadComponent
   ],
   imports: [
     BrowserModule,
+    StoreModule.forRoot({
+      todo: reducer
+    }),
     AppRoutingModule
   ],
   providers: [],
